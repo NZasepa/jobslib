7fb04d9d8b011dc8cb0040a99191a15961c87354
NZasepa <zasepanatan@gmail.com>
2017-01-31 18:43:33 +0100

    Shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Natan on 31.01.2017.
+ */
+import { NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [CommonModule, FormsModule],
+  declarations: []
+})
+export class SharedModule {}
