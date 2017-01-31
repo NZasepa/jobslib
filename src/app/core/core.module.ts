9c4068c472cb944deca62f23765961fbd72d759f
NZasepa <zasepanatan@gmail.com>
2017-01-31 23:10:16 +0100

    Base file structure and header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,19 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from "@angular/common";
 
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
 @NgModule({
   imports: [CommonModule],
-  exports: [],
-  declarations: [],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   providers: [],
 })
 export class CoreModule {
