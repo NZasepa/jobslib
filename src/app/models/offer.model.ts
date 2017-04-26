fbdecf4a6bd0aae7a82465065a1abc31eb7b15fd
Natan Zasępa <zasepanatan@gmail.com>
2017-04-26 08:35:55 +0200

    Added ngrx store

---
+++ src/app/models/offer.model.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by kbsystemsmac on 25.04.2017.
+ */
+
+export interface Offer {
+  title: string;
+  description?: string;
+  url: string;
+  company?: string;
+  site?: string;
+  salary?: string;
+  date?: Date;
+}
